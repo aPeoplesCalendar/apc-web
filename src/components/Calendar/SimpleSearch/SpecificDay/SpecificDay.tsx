e1b373ff178a0f00931bec52fbffdfde8e3de2e1
Trevor Smith <tts2p4@gmail.com>
2023-02-12 21:01:58 -0600

    removed rendundant todos

--- src/components/Calendar/SimpleSearch/SpecificDay/SpecificDay.tsx
+++ src/components/Calendar/SimpleSearch/SpecificDay/SpecificDay.tsx
@@ -6,18 +6,6 @@ import { QueryResultEventDisplay } from "../../QueryResultEventDisplay/QueryResu
 import * as styles from "./SpecificDay.styles";
 import { fetchEvents } from "./SpecificDay.utils";
 
-// to dos
-// render a daily, weekly, or monthly date picker for each calendar mode
-// render a different UI for each mode - what does a week's worth of events look like? a month?
-// map social media share icons on each queried event (right side when big, bottom when small?)
-// social media share icons on each specific event
-// add suggest an event page?? - what do the RLS policies look like?
-// finalize color scheme
-// write the meta stuff - about page, contact page, homepage, readme
-// website meta stuff - icons, meta tags, lock down github repo
-// deploy via netlify
-// update apc-form to work with new stuff
-
 export const SpecificDay = ({ month, day }: { month: string; day: string }) => {
   const [events, setEvents] = useState<DatabaseEvent[] | null>();
   const [loading, setLoading] = useState(false);
